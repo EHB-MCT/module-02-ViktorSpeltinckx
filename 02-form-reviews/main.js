9838d05d17a39361e813bc9f8f513107fbe8491e
ViktorSpeltinckx <viktor.speltinckx@student.ehb.be>
2026-03-12 13:41:34 +0100

    revieuws

--- 02-form-reviews/main.js
+++ 02-form-reviews/main.js
@@ -19,11 +19,42 @@ let reviews = [
 ];
 
 // 2. Select DOM Elements
+let container = document.querySelector("#reviews-container");
+
+let form = document.querySelector("#review-form");
+let nameInput = document.querySelector("#name");
+let ratingInput = document.querySelector("#rating");
+let reviewInput = document.querySelector("#review");
+
 
 // 3. Render Function
 // This function should render the reviews array, based on the template in the HTML file.
 function renderReviews() {
     console.log("Render the reviews");
+
+    // clear container
+    container.innerHTML = "";
+
+    // loop through reviews
+    for (let review of reviews) {
+
+        // create stars
+        let stars = "⭐".repeat(review.rating);
+
+        // create div
+        let div = document.createElement("div");
+        div.classList.add("review-item");
+
+        div.innerHTML = `
+            <div class="review-header">
+                <span class="review-name">${review.name}</span>
+                <span class="review-rating">${stars}</span>
+            </div>
+            <p class="review-text">${review.text}</p>
+        `;
+
+        container.appendChild(div);
+    }
 }
 
 // 4. Initial Render
@@ -31,13 +62,23 @@ function renderReviews() {
 renderReviews();
 
 // 5. Handle Form Submission
-// 5.1 Stop the page from reloading!
+form.addEventListener("submit", function (e) {
 
-// 5.2 Create a new object from input values
+    // 5.1 Stop the page from reloading
+    e.preventDefault();
 
-// 5.3 Add (push) the new object to our data array
+    // 5.2 Create a new object from input values
+    let newReview = {
+        name: nameInput.value,
+        rating: Number(ratingInput.value),
+        text: reviewInput.value
+    };
+    // 5.3 Add (push) the new object to our data array
+    reviews.push(newReview);
 
-// 5.4 Re-render the list to show the new item
+    // 5.4 Re-render the list to show the new item
+    renderReviews();
 
-// Optional: Clear the form fields
-
+    // Optional: Clear the form fields
+    form.reset();
+});
